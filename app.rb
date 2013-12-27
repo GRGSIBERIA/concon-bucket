b1b24fc972c026dd89a37df6477c0342e205d027
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-12-27 19:49:43 +0900

    ルーティングする

--- app.rb
+++ app.rb
@@ -1,4 +1,41 @@
 #-*- encoding: utf-8
-require './dynamodb.rb'
+require "sinatra"
+require "sinatra/reloader"
+require "aws-sdk"
+require "slim"
 
-map = Mapper.new
+require "./dynamodb.rb"
+
+class Application < Sinatra::Base
+  set :sessions, true
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    slim :index
+  end
+
+  # ログイン
+  get '/signin' do
+    slim :signin
+  end
+
+  # ユーザ登録
+  get '/signup' do
+    slim :signup
+  end
+
+  # ユーザ登録処理
+  post '/signup_procedure' do
+
+  end
+
+  # ユーザのログイン認証
+  post '/authenticate_user' do
+
+  end
+end
+
+Application.run! :host => 'localhost', :port => 4567
