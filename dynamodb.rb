0f708e952690ba8c1687e02e3da2ac809d3a24ec
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-12-27 19:49:55 +0900

    静的なメンバはよくないと思った

--- dynamodb.rb
+++ dynamodb.rb
@@ -1,18 +1,18 @@
 #-*- encoding: utf-8
 require 'aws-sdk'
 
-class Mapper
+class DynamoDB
   def initialize
     account = []
     open('./dynamo.conf').each {|x| account << x}.close
 
-    @@db = AWS::DynamoDB.new(
+    @db = AWS::DynamoDB.new(
       :access_key_id => account[0],
       :secret_access_key => account[1])
     puts "Successfully connect to DynamoDB: #{@db}"
   end
 
-  def self.db
-    @@db
+  def db
+    @db
   end
 end
