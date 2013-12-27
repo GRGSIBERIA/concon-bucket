ca192aaf015566ca39f78d414682a041a27b0b39
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-12-27 16:53:46 +0900

    dynamodbつくる

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
 #-*- encoding: utf-8
 require './dynamodb.rb'
 
-map = DMapper.new
+map = Mapper.new
--- dynamodb.rb
+++ dynamodb.rb
@@ -1,14 +1,18 @@
 #-*- encoding: utf-8
 require 'aws-sdk'
 
-class DMapper
+class Mapper
   def initialize
     account = []
     open('./dynamo.conf').each {|x| account << x}.close
 
-    @db = AWS::DynamoDB.new(
+    @@db = AWS::DynamoDB.new(
       :access_key_id => account[0],
       :secret_access_key => account[1])
-    puts "successfully connect DynamoDB"
+    puts "Successfully connect to DynamoDB: #{@db}"
+  end
+
+  def self.db
+    @@db
   end
 end
