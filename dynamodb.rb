dbbfe489fd51ecc3aae6c6a51d4dd5ad41bfad89
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-12-27 15:22:58 +0900

    Coreなクラスを作る

--- dynamodb.rb
+++ dynamodb.rb
@@ -1,8 +1,13 @@
 #-*- encoding: utf-8
+require 'aws-sdk'
 
-account = []
-open('./dynamo.conf').each {|x| account << x}.close
+class DMapper
+  def init
+    account = []
+    open('./dynamo.conf').each {|x| account << x}.close
 
-dynamo_db = AWS::DynamoDB.new(
-  :access_key_id => account[0],
-  :secret_access_key => account[1])
+    @db = AWS::DynamoDB.new(
+      :access_key_id => account[0],
+      :secret_access_key => account[1])
+  end
+end
